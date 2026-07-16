ba8c6d9dd19c4b43b1966a7e701cdfec4822573b
noah-coder840 <ghadry1387@gmail.com>
2026-07-16 17:20:36 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-for(let i=0;i<50;i++){
-
-    let star=document.createElement("div");
-
-    star.innerHTML="✦";
-    star.classList.add("star");
-
-    star.style.left=Math.random()*100+"vw";
-    star.style.fontSize=(Math.random()*20+10)+"px";
-    star.style.animationDuration=(Math.random()*5+5)+"s";
-
+function createStar() {
+    const star = document.createElement("div");
+    star.innerHTML = "✦";
+    star.style.position = "fixed";
+    star.style.left = Math.random() * window.innerWidth + "px";
+    star.style.top = "-20px";
+    star.style.color = "silver";
+    star.style.fontSize = Math.random() * 15 + 10 + "px";
+    star.style.pointerEvents = "none";
     document.body.appendChild(star);
+
+    let pos = -20;
+
+    const fall = setInterval(() => {
+        pos += 3;
+        star.style.top = pos + "px";
+
+        if (pos > window.innerHeight) {
+            clearInterval(fall);
+            star.remove();
+        }
+    }, 20);
 }
+
+setInterval(createStar, 200);
