11d7b4ff723dfc39238d57dac9246ea547e64ae8
noah-coder840 <ghadry1387@gmail.com>
2026-07-16 16:08:11 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+for(let i=0;i<50;i++){
 
+    let star=document.createElement("div");
+
+    star.innerHTML="✦";
+    star.classList.add("star");
+
+    star.style.left=Math.random()*100+"vw";
+    star.style.fontSize=(Math.random()*20+10)+"px";
+    star.style.animationDuration=(Math.random()*5+5)+"s";
+
+    document.body.appendChild(star);
+}
